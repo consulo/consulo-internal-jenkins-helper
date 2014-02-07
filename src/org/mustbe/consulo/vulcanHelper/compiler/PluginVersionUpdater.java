f77a4753abbc25d2e6dd1e10f6911218fee692cc
VISTALL <vistall.valeriy@gmail.com>
2014-02-07 10:33:34 +0200

    added checks for compiler. For example - dont allow compilation if 1.6 bytecode target not set. Due later plugin ill not work on 1.6 JDK

--- src/org/mustbe/consulo/vulcanHelper/compiler/PluginVersionUpdater.java
+++ src/org/mustbe/consulo/vulcanHelper/compiler/PluginVersionUpdater.java
@@ -13,6 +13,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.mustbe.consulo.roots.impl.ProductionContentFolderTypeProvider;
 import org.mustbe.consulo.roots.impl.ProductionResourceContentFolderTypeProvider;
+import com.intellij.compiler.impl.javaCompiler.JavaCompilerConfiguration;
 import com.intellij.openapi.compiler.CompileContext;
 import com.intellij.openapi.compiler.CompileScope;
 import com.intellij.openapi.compiler.CompilerManager;
@@ -21,8 +22,11 @@ import com.intellij.openapi.compiler.Validator;
 import com.intellij.openapi.compiler.ValidityState;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.JDOMUtil;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.uiDesigner.GuiDesignerConfiguration;
 
 /**
  * @author VISTALL
@@ -58,15 +62,22 @@ public class PluginVersionUpdater implements Validator
 	private static final String META_INF_PLUGIN_XML = "META-INF/plugin.xml";
 
 	private static final String BUILD_NUMBER = System.getProperty("vulcan.build.number");
+	private final Project myProject;
+
+	public PluginVersionUpdater(Project project)
+	{
+		myProject = project;
+	}
 
 	@NotNull
 	@Override
 	public ProcessingItem[] getProcessingItems(CompileContext compileContext)
 	{
-		if(BUILD_NUMBER == null)
+		if(!isConsuloOrganizationProject() || BUILD_NUMBER == null)
 		{
 			return ProcessingItem.EMPTY_ARRAY;
 		}
+
 		List<ProcessingItem> list = new ArrayList<ProcessingItem>();
 		ModuleManager moduleManager = ModuleManager.getInstance(compileContext.getProject());
 		for(Module module : moduleManager.getModules())
@@ -100,6 +111,11 @@ public class PluginVersionUpdater implements Validator
 	@Override
 	public ProcessingItem[] process(CompileContext compileContext, ProcessingItem[] processingItems)
 	{
+		if(!isConsuloOrganizationProject())
+		{
+			return ProcessingItem.EMPTY_ARRAY;
+		}
+
 		for(ProcessingItem processingItem : processingItems)
 		{
 			try
@@ -144,6 +160,36 @@ public class PluginVersionUpdater implements Validator
 	@Override
 	public boolean validateConfiguration(CompileScope compileScope)
 	{
+		if(!isConsuloOrganizationProject())
+		{
+			return true;
+		}
+
+		JavaCompilerConfiguration javaCompilerConfiguration = JavaCompilerConfiguration.getInstance(myProject);
+		if(!Comparing.equal(javaCompilerConfiguration.getProjectBytecodeTarget(), "1.6"))
+		{
+			LOGGER.error("Java: Bytecode target is not specified or wrong. Need '1.6'");
+			return false;
+		}
+
+		if(!javaCompilerConfiguration.isAddNotNullAssertions())
+		{
+			LOGGER.error("Java: '@NotNull' asserting is disabled. Enable it");
+			return false;
+		}
+
+		GuiDesignerConfiguration guiDesignerConfiguration = GuiDesignerConfiguration.getInstance(myProject);
+		if(guiDesignerConfiguration.COPY_FORMS_RUNTIME_TO_OUTPUT)
+		{
+			LOGGER.error("UI Designer: 'Copy forms runtime to output' is enabled. Disable it");
+			return false;
+		}
+
+		if(!guiDesignerConfiguration.COPY_FORMS_TO_OUTPUT)
+		{
+			LOGGER.error("UI Designer: 'Copy forms to output' is disabled. Enable it");
+			return false;
+		}
 		return true;
 	}
 
@@ -157,5 +203,10 @@ public class PluginVersionUpdater implements Validator
 	public ValidityState createValidityState(DataInput dataInput) throws IOException
 	{
 		return new EmptyValidityState();
+	}
+
+	private boolean isConsuloOrganizationProject()
+	{
+		return myProject.getName().startsWith("consulo");
 	}
 }
